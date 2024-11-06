5558f033b1e5b51bffb0f1f3ba902775f247e83a
cjillian <jillian.castro@edu.sait.ca>
2024-11-06 16:34:00 -0700

    js button image switch

---
+++ intro.js
@@ -0,0 +1,19 @@
+
+sheep.addEventListener('click',function(){
+    console.log("sheep goes mehhh");
+	document.getElementById('hero').src="sheep.webp";
+})
+
+bear.addEventListener('click',function(){
+    console.log("bear goes ahuuarruuur");
+	document.getElementById('hero').src="bear.webp";
+})
+wolf.addEventListener('click',function(){
+    console.log("wolf goes aaooooooo");
+	document.getElementById('hero').src="wolf.webp";
+})
+elk.addEventListener('click',function(){
+    console.log("elk goes idk");
+	document.getElementById('hero').src="elk.webp";
+})
+
